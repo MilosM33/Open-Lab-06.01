ab67d821139a61d915af2e2d13e951e077b05f67
MilosM33 <milosmarejka89@gmail.com>
2020-10-09 23:09:20 +0200

    init

--- Open-Lab-06.01/Challenge.cs
+++ Open-Lab-06.01/Challenge.cs
@@ -6,7 +6,19 @@ namespace Open_Lab_06._01
     {
         public string MysteryFunc(string str)
         {
-            throw new NotImplementedException();
+            string temp = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str.Length> i+1)
+                {
+                    for (int x = 0; x < int.Parse(str[i+1].ToString()); x++)
+                    {
+                        temp += str[i];
+                    }
+                }
+                i++;
+            }
+            return temp;
         }
     }
 }
